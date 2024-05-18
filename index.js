8c13024bc3f8fbc2d9c46f85182a79305aa76206
Sára Kraibich Tesařová <sara.k.tesarova@gmail.com>
2024-05-18 12:41:11 +0200

    Ukol 3 - dovednosti

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('funguju!');
+
+const dovednostHTML = prompt('Zadejte úroveň dovednosti v HTML (0-100):')
+const dovednostCSS = prompt('Zadejte úroveň dovednosti v CSS (0-100):')
+const dovednostJS = prompt('Zadejte úroveň dovednosti v JavaScript (0-100):')
+
+
+const updateSkill = (elementID,hodnotaSkillu) => {
+    const element = document.querySelector(elementID)
+
+    if (hodnotaSkillu >= 0 && hodnotaSkillu <= 100) {
+        const elProgress = element.querySelector('.skill__progress')
+        elProgress.style.width = hodnotaSkillu + '%'
+    } else {
+        document.body.innerHTML = 'Zadejte hodnoty mezi 0 a 100'
+    }
+        const elValue = element.querySelector('.skill__value')
+        elValue.textContent = hodnotaSkillu + '/ 100'
+    } 
+
+
+updateSkill('#skill1',dovednostHTML)
+updateSkill('#skill2',dovednostCSS)
+updateSkill('#skill3',dovednostJS)
